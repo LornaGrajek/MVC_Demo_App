52377dc6cf2be44ecfe1ec6aacfc01650a4c7833
LornaGrajek <lorna.grajek@gmail.com>
2022-04-27 10:37:32 -0700

    Set up a DBcontext Model

---
+++ Models/MyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; //This namespace is crucial for migrations
+
+//go to Project on the top tool bar, Manage nuget packages, and search for EF Core (Entity Framework) and install
+//go to Tools, nuget package manager, package manager console
+//in the console, type enable-migrations to.... enable migrations
+//to create a migration, type, add-migration <name-of-migration>
+namespace StoreApp.Models
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext() { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+    }
+}

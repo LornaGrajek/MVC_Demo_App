9c782579d9ae25535872327a222f4954dfb12f64
LornaGrajek <lorna.grajek@gmail.com>
2022-04-29 08:11:48 -0700

    dependency injection

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -10,21 +10,41 @@ namespace StoreApp.Controllers
 {
     public class CustomerController : Controller
     {
+
+        private MyDBContext _context; //dependency injection
+        public CustomerController()
+        {
+            _context = new MyDBContext(); //disposable object
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer
         public ActionResult Users()
         {
-            var customers = GetCustomers();
+            // var customers = GetCustomers(); we used this method when we were hardcoding our data
+
+            //we are using dependency injection to retreive customer information from our dbcontext (database)
+            // the dbcontext does not query the database, this is called deferred execution
+            //the database gets queried when we iterate over the customers variable
+            var customers = _context.Customers; 
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
             //Single or default returns the only vaule in a sequence (list) that matches a parameter (id)
             //lamda expression, input parameter is 'c' for customer, the expression looks for customer id that matches id
 
-            if(customer == null)
+            // using dependency injecton
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
---
+++ Migrations/202204291341358_Second.cs
@@ -0,0 +1,17 @@
+﻿namespace StoreApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Second : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Customers (UserName, Password) VALUES ('Lorna', 'password')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
